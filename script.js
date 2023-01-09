07f89bfe69fb3e4745fada470218e8216f367044
Sam Lloyd <sam.thomas.lloyd@gmail.com>
2023-01-09 21:53:10 +0000

    dev/ colour swap button added

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 
 document.getElementById("fauxSite").innerHTML = `
 <h2> EYYYY </h2>`
+
+
+const colourButton = document.getElementById("colourButton");
+
+colourButton.addEventListener("click", textColSwap);
+
+function textColSwap(){
+  let text = document.getElementsByClassName("textClass");
+
+  for (var i = 0; i < text.length; i++) {
+    text[i].style.color = "blue";
+  }
+
+}
