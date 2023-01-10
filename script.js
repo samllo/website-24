7ece4a886000d3caf54554629691c644e954320f
Sam Lloyd <sam.thomas.lloyd@gmail.com>
2023-01-10 21:07:23 +0000

    dev/ colour change added, basic styling

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 
-document.getElementById("fauxSite").innerHTML = `
-<h2> EYYYY </h2>`
+// document.getElementById("fauxSite").innerHTML = `
+// <h2> EYYYY </h2>`
 
-
+let bool = true;
 const colourButton = document.getElementById("colourButton");
-
 colourButton.addEventListener("click", textColSwap);
-
 function textColSwap(){
-  let text = document.getElementsByClassName("textClass");
-
-  for (var i = 0; i < text.length; i++) {
-    text[i].style.color = "blue";
+  let textColour;
+  let bgColour;
+  if (bool){
+    textColour = "#E63946";
+    bgColour = "#F1FAEE";
+  }else{
+    textColour = "#1D3557";
+    bgColour = "#A8DADC";
   }
 
+  let text = document.getElementsByClassName("textClass");
+  for (var i = 0; i < text.length; i++) {
+    text[i].style.color = textColour;
+  }
+
+  let bg = document.getElementById("bgClass");
+ 
+  bg.style.backgroundColor = bgColour;
+  bool = !bool;
 }
